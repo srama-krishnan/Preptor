a3a0bb476249f4b0fede37fc9d4542d54f06a4fd
S Ramakrishnan <sramakrishnan1841@gmail.com>
2025-07-08 12:12:04 +0530

    Basic Setup of static files and dependencies

---
+++ components/FormField.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FormControl, FormDescription, FormItem, FormMessage, FormLabel } from './ui/form';
+import { Input } from './ui/input';
+import { Controller, FieldValues, Control, Path } from 'react-hook-form';
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password";
+}
+
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+              className="input"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormField;
