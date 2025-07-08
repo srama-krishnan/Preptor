332d78dd015d470a66ac979186b79388e21e23aa
S Ramakrishnan <sramakrishnan1841@gmail.com>
2025-07-08 12:13:22 +0530

    Authorization Pages Routing

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthForm from "@/components/AuthForm";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <AuthForm type = "sign-up"/>
+  )
+}
+
+export default page
