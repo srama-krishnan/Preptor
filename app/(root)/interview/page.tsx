7ffa56e975dea2bcbf1bbaa8e6716c793c244069
S Ramakrishnan <sramakrishnan1841@gmail.com>
2025-07-10 11:02:35 +0530

    implement AI Agent

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,15 @@
-import Agents from '@/components/Agents'
-import React from 'react'
+import Agent from "@/components/Agent";
+import {getCurrentUser} from "@/lib/actions/auth.action";
 
-const page = () => {
-  return (
-    <>
-        <h3>Interview Generation</h3>
-        <Agents userName="You" userId="user1" type="generate"/>
-    </>
-  )
+const Page = async () => {
+    const user = await getCurrentUser();
+
+    return (
+        <>
+            <h3>Interview Generation</h3>
+
+            <Agent userName={user?.name || ''} userId={user?.id} type="generate" />
+        </>
+    )
 }
-
-export default page
+export default Page
